3bc8384e4479aa2a1248555380d57405362fe1ed
Titto Nugraha Auladhana <tittonugrahaa@gmail.com>
2023-11-22 12:18:18 +0700

    Latihan_laravelweb

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/input', [BarangController::class, 'formInput']);
 Route::post('/hasil', [BarangController::class, 'formHasil']);
+
+Route::get('/coba', [BarangController::class, 'index']);
